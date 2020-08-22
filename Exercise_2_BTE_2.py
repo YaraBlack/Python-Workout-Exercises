e4d7c6a3a455bb5d59e6e8f531f5e13c055f3855
YaraBlack <yara26072000@gmail.com>
2020-08-22 17:31:43 +0300

    Done

---
+++ Exercise_2_BTE_2.py
@@ -0,0 +1,21 @@
+#Summing numbers
+'''
+Summ all numbers that is inputed in the function
+'''
+
+#Beyond the exercise 2 (BTE_2)
+'''
+Write a function that takes a list of numbers. It should return the average (i.e.,
+arithmetic mean) of those numbers.
+'''
+
+def myavg(numbers):
+    sum = 0
+    quantity = 0
+    for i in numbers:
+        quantity += 1
+        sum += i
+    return sum/quantity
+
+
+print('{:.2f}'.format(myavg([1, 2, 5])))
