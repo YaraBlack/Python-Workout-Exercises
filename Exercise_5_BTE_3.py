10a2c03f7db00e446ff8fae75c113dd3b29c5871
YaraBlack <yara26072000@gmail.com>
2022-01-22 22:35:50 +0200

    To modify the vowels searching

--- Exercise_5_BTE_3.py
+++ Exercise_5_BTE_3.py
@@ -7,7 +7,7 @@ word. So “air” becomes “airway” and “eat” becomes “eatway.”
 - If the word begins with any other letter, then we take the first letter, put it on
 the end of the word, and then add “ay.” Thus, “python” becomes “ythonpay”
 and “computer” becomes “omputercay.”
-(And yes, I recognize that the rules can be made more sophisticated. Let’s keep it simple
+(And yes, I recognize that the rules can be made more sophisticated. Let's keep it simple
 for the purposes of this exercise.)
 For this exercise, write a Python function (pig_latin) that takes a string as input,
 assumed to be an English word. The function should return the translation of this word
@@ -16,10 +16,10 @@ into Pig Latin. You may assume that the word contains no capital letters or punc
 
 #Beyond the exercise 3 (BTE_3)
 '''
-Consider an alternative version of Pig Latin—We don’t check to see if the first letter
+Consider an alternative version of Pig Latin—We don't check to see if the first letter
 is a vowel, but, rather, we check to see if the word contains two different vowels.
-If it does, we don’t move the first letter to the end. Because the word “wine”
-contains two different vowels (“i” and “e”), we’ll add “way” to the end of it, giving
+If it does, we don't move the first letter to the end. Because the word “wine”
+contains two different vowels (“i” and “e”), we'll add “way” to the end of it, giving
 us “wineway.” By contrast, the word “wind” contains only one vowel, so we
 would move the first letter to the end and add “ay,” rendering it “indway.” How
 would you check for two different vowels in the word? (Hint: sets can come in
