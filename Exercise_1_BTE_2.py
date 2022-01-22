241c2f78f481bed247ad4ff061b064f5dc6c0d27
YaraBlack <yara26072000@gmail.com>
2022-01-22 20:10:12 +0200

    The 2nd "Beyond the exercise" task to the 1st exercise
    Added int_to_base function, replaced int(str(number), base) with it

--- Exercise_1_BTE_2.py
+++ Exercise_1_BTE_2.py
@@ -17,19 +17,39 @@ try again.
 '''
 Not only should you choose a random number, but you should also choose a
 random number base, from 2 to 16, in which the user should submit their
-input. If the user inputs “10” as their guess, you’ll need to interpret it in the
+input. If the user inputs “10” as their guess, you`ll need to interpret it in the
 correct number base; “10” might mean 10 (decimal), or 2 (binary), or 16
 (hexadecimal).
 '''
 
 import random
+import string
 
+def int_to_base(n, N):      #converts a number to another base
+    base_n_digits = string.digits + string.ascii_lowercase + string.ascii_uppercase
+    result = ""
+    # if n < 0:
+    #     sign = "-"
+    #     n = -n
+    # else:
+    #     sign = ""
+    while n > 0:
+        q, r = divmod(n, N)
+        result += base_n_digits[r]
+        n = q
+    if result == "":
+        result = "0"
+    return int("".join(reversed(result)))
+    # return sign + "".join(reversed(result))
 
 def GuessNumber():
     num_base = random.choice([i for i in range(2,17)])
-    number = int(str(random.randint(0,100)), base = num_base)
-    while True:
-        userInput = int(input("Guess a number: "), base = num_base)
+    number = int_to_base(random.randint(0,100), num_base)
+    print ("The number base is: {}".format(num_base))
+    while True:      
+        userInput = int_to_base(int(input("Guess a number: ")), num_base)
+        print ("Your number in base-{} is: {}".format(num_base, userInput))
+        
         if userInput == number:
             print("Just right.")
             break
