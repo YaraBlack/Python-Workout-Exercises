6d0d62d937bda524105dd5fcaad165e7a092848c
YaraBlack <yara26072000@gmail.com>
2022-01-22 22:22:43 +0200

    The 2nd "Beyond the exercise" task to the 2nd exercise

--- Exercise_3_BTE_2.py
+++ Exercise_3_BTE_2.py
@@ -1,6 +1,6 @@
 #Run timing
 '''
-For this exercise, then, we’ll assume that you run 10 km each day as part of your
+For this exercise, then, we'll assume that you run 10 km each day as part of your
 exercise regime. You want to know how long, on average, that run takes.
 Write a function (run_timing) that asks how long it took for you to run 10 km.
 The function continues to ask how long (in minutes) it took for additional runs, until
@@ -12,9 +12,9 @@ Note that the numeric inputs and outputs should all be floating-point values.
 #Beyond the exercise 2 (BTE_2)
 '''
 Explore the Decimal class (http://mng.bz/oPVr), which has an alternative
-floating-point representation that’s as accurate as any decimal number can be.
+floating-point representation that's as accurate as any decimal number can be.
 Write a function that takes two strings from the user, turns them into decimal
-instances, and then prints the floating-point sum of the user’s two inputs. In
+instances, and then prints the floating-point sum of the user's two inputs. In
 other words, make it possible for the user to enter 0.1 and 0.2, and for us to get
 0.3 back.
 '''
