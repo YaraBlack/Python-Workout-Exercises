dc29681b314bb7baed8fb6fd0d221ff4fee18dfb
YaraBlack <yara26072000@gmail.com>
2022-01-22 22:23:00 +0200

    The 2nd "Beyond the exercise" task to the 4th exercise

--- Exercise_4_BTE_2.py
+++ Exercise_4_BTE_2.py
@@ -1,11 +1,11 @@
 #Hexadecimal output
 '''
-In this exercise, you’ll see how a bit of creativity, along with the built-in 'reversed'
+In this exercise, you'll see how a bit of creativity, along with the built-in 'reversed'
 and 'enumerate' functions, can help you to get around issues.
 For this exercise, you need to write a function (hex_output) that takes a hex number
-and returns the decimal equivalent. That is, if the user enters 50, you’ll assume
-that it’s a hex number (equal to 0x50) and will print the value 80 to the screen. And
-no, you shouldn’t convert the number all at once using the int function, although it’s
+and returns the decimal equivalent. That is, if the user enters 50, you'll assume
+that it's a hex number (equal to 0x50) and will print the value 80 to the screen. And
+no, you shouldn't convert the number all at once using the int function, although it's
 permissible to use int one digit at a time.
 '''
 
