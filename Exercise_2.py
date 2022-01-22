8658e9b87d0e42a46e42a692428dfbe4cf1db695
YaraBlack <yara26072000@gmail.com>
2022-01-22 20:15:39 +0200

    The 2nd exercise w/o "Beyond the exercise"

--- Exercise_2.py
+++ Exercise_2.py
@@ -10,9 +10,9 @@ def mysum(*args):
     return sum
 
 
-print(mysum(1, 2, 3))
+#print(mysum(1, 2, 3))
 '''
 If we call mysum(*[1,2,3]), our list becomes three separate arguments, which will
 then allow the function to be called in the usual way.
 '''
-#print(mysum(* [1, 2, 3]))
+print(mysum(* [1, 2, 3]))
